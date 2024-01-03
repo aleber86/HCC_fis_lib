35339dd1e32708177f97fb6233494171ad5d0251
Alexis <alexisgberlot@gmail.com>
2024-01-03 11:24:30 -0300

    Position args dims 2,3 -> 3, / Change in func: get_position, set_position; func added get_angular_position, set_angular_position

--- src/cube.py
+++ src/cube.py
@@ -4,11 +4,21 @@ from cilinder import Cilinder
 
 class Cube(Cilinder):
     """Primitive mesh body. Child of Cilinder class."""
-    def __init__(self, side_size, mass, friction, init_pos, init_vel, init_rot, destructive, _wp = np.float64):
-        Cilinder.__init__(self, side_size, side_size,4 ,mass, friction, init_pos, init_vel, init_rot, destructive, _wp = _wp )
+    def __init__(self, side_size, mass, friction,
+                 init_pos, init_angular, init_vel,
+                 init_rot, destructive, _wp = np.float64):
+
+        Cilinder.__init__(self, side_size, side_size,4 ,mass,
+                          friction, init_pos, init_angular,
+                          init_vel, init_rot, destructive, _wp = _wp )
 
 
 class Prisma(Cilinder):
     """Primitive mesh body. Child of Cilinder class."""
-    def __init__(self, side_size, height, mass, friction, init_pos, init_vel, init_rot, destructive, _wp = np.float64):
-        Cilinder.__init__(self, side_size, height, 3 ,mass, friction, init_pos, init_vel, init_rot, destructive, _wp = _wp )
+    def __init__(self, side_size, height, mass, friction,
+                 init_pos, init_angular,init_vel, init_rot,
+                 destructive, _wp = np.float64):
+
+        Cilinder.__init__(self, side_size, height, 3 ,mass,
+                          friction, init_pos, init_angular, init_vel,
+                          init_rot, destructive, _wp = _wp )
