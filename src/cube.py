a2998c3b18b5cd6381a9d7b12f2f90004d7ec148
Alexis <alexisgberlot@gmail.com>
2024-01-06 11:19:17 -0300

    New definition point-face collision function. Definition inertia tensor (Cube, Cilinder)

--- src/cube.py
+++ src/cube.py
@@ -11,7 +11,11 @@ class Cube(Cilinder):
         Cilinder.__init__(self, side_size, side_size,4 ,mass,
                           friction, init_pos, init_angular,
                           init_vel, init_rot, destructive, _wp = _wp )
-
+    def __inertia_tensor(self):
+        diag_1_1 = 1/6*self.mass*(self.size**2)
+        diag_2_2 = diag_1_1
+        diag_3_3 = diag_1_1
+        self.inertia_tensor = np.array([[diag_1_1,0,0],[0,diag_2_2,0],[0,0,diag_3_3]])
 
 class Prisma(Cilinder):
     """Primitive mesh body. Child of Cilinder class."""
